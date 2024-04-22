e19129f206501bbd39c3e4c1f718442a9dfa0760
Michael Dorst <m@mdorst.net>
2024-04-22 16:38:16 -0700

    Change cmp next/prev/confirm to <C-j>/<C-k>/<C-;>

--- init.lua
+++ init.lua
@@ -46,9 +46,6 @@ vim.opt.wrap = true
 
 -- vim.keymap.set('n', 's', '<Nop>')
 
--- Leave insert mode
-vim.keymap.set('i', '<C-;>', '<Esc>')
-
 -- Redo
 vim.keymap.set('n', 'R', '<C-r>')
 
@@ -529,9 +526,9 @@ require('lazy').setup({
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -540,7 +537,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-;>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
